4b5ed6eb64af630ecb68f2146897ca1ef3a1e8e5
Christopher Larios <95603702+chrisLarios@users.noreply.github.com>
2022-08-23 18:25:41 -0500

    handling InfinitePeople using useInfiniteQuery

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -9,5 +10,42 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    { getNextPageParam: (lastPage) => lastPage.next || undefined }
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>we have an Error: {error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">Fetching...</div>}
+      <InfiniteScroll
+        data={data}
+        loadMore={fetchNextPage}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) =>
+          pageData.results.map((it) => (
+            <Person
+              key={it.key}
+              name={it.name}
+              hairColor={it.hair_color}
+              eyeColor={it.eye_color}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
+  );
 }
