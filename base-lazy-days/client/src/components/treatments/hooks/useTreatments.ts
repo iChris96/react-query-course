0acdc530a9ba45118de027e7e1d8a5c30ca7876b
Christopher Larios <95603702+chrisLarios@users.noreply.github.com>
2022-08-28 20:32:40 -0500

    usePrefetchTreatments hook

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -15,4 +15,9 @@ export function useTreatments(): Treatment[] {
   const { data = [] } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient(); // we need to get our queryClient
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
