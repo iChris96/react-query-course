ca030f935a85b9cb9a84a4af89a14e63550ba713
Christopher Larios <95603702+chrisLarios@users.noreply.github.com>
2022-09-06 20:37:06 -0500

    adding a QueryClientProvider for our tests

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 
+import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
 test('renders response from query', () => {
   // write test here
-  render(<Treatments />);
+  renderWithQueryClient(<Treatments />);
 });
--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,11 +1,26 @@
-import { render } from '@testing-library/react';
-
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
-// from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+// // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 // export const createQueryClientWrapper = (): React.FC => {
 //   const queryClient = generateQueryClient();
 //   return ({ children }) => (
 //     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 //   );
 // };
+
+export const renderWithQueryClient = (
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult => {
+  const queryClient = client ?? generateQueryClient();
+
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+};
