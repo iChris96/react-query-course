3f1868b3b52a60bf28dc90492c9521ee1923f8e7
Christopher Larios <95603702+chrisLarios@users.noreply.github.com>
2022-09-06 21:01:26 -0500

    using our wrap query provider and verify if we are getting staff & treatments names correctly

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,7 +3,13 @@ import { render, screen } from '@testing-library/react';
 import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
   // write test here
   renderWithQueryClient(<Treatments />);
+
+  const treatmentTitles = await screen.findAllByRole('heading', {
+    name: /massage|facial|scrub/i,
+  });
+
+  expect(treatmentTitles).toHaveLength(3);
 });
