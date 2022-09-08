49d1188b912000c6acb3f84b960402a058abfcbc
Christopher Larios <95603702+chrisLarios@users.noreply.github.com>
2022-09-08 00:59:30 -0500

    testing a custom hook using useStaff hook

--- base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,5 +1,24 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { createQueryClientWrapper } from 'test-utils';
+
 import { useStaff } from '../hooks/useStaff';
 
 test('filter staff', async () => {
   // the magic happens here
+  const { result, waitFor } = renderHook(useStaff, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for the staff to be 4
+  await waitFor(() => Object.keys(result.current.staff).length === 4); // Dyvyam Sandra, Michael & Mateo
+
+  // update the filter state value
+  act(() => result.current.setFilter('scrub'));
+
+  // expect the staff length to be appropriate for the new filter state
+  await waitFor(() => {
+    return (
+      Object.keys(result.current.staff).length === 2 // we should have only Divya & Michael now
+    );
+  });
 });
