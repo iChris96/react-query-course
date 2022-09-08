5606dcabefea112e0998dc88aeb4e585fc302b94
Christopher Larios <95603702+chrisLarios@users.noreply.github.com>
2022-09-08 00:05:47 -0500

    testing AllStaff with an error request and testing mutations for a new appointment

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,9 +1,9 @@
 import { screen } from '@testing-library/react';
+import { rest } from 'msw';
 
-import { renderWithQueryClient } from '../../../test-utils';
-// import { rest } from 'msw';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
+import { server } from '../../../mocks/server';
+import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
 test('renders response from query', async () => {
@@ -18,9 +18,14 @@ test('renders response from query', async () => {
 
 test('handles query error', async () => {
   // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.resetHandlers(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
+
+  renderWithQueryClient(<AllStaff />);
+
+  const toast = await screen.findByRole('alert');
+  expect(toast).toHaveTextContent('Request failed with status code 500');
 });
